ec181b01431ed1cc49ebd061bfe6d3dd7943fd77
Daniel J. Hofmann <daniel@trvx.org>
2012-05-10 22:31:47 +0200

    refactoring, srtm model enum

---
+++ SRTMModel.h
@@ -0,0 +1,27 @@
+#ifndef SRTMMODEL_H
+#define SRTMMODEL_H
+
+
+namespace SRTMUtil {
+
+
+/* available SRTM models */
+enum class SRTMModel : long int {
+  /*
+   * model 1: U.S.,
+   * sampled at one arc-second lat/long intervals, 3601 lines, 3601 samples
+   */
+  US = 3601L,
+
+  /*
+   * model 3: world,
+   * sampled at three arc-second lat/long intervals, 1201, 1201 samples
+   */
+  World = 1201L
+};
+
+
+} // namespace
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 #include <fstream>
 
+#include "SRTMModel.h"
 #include "HGTParser.h"
+#include "TileExporter.h"
+#include "TileSurface.h"
 
 using std::cout;
 using std::cerr;
 using std::endl;
 
 using SRTMUtil::HGTParser;
+using SRTMUtil::SRTMModel;
+using SRTMUtil::TileExporter;
+using SRTMUtil::TileSurface;
 
 using namespace std;
 
 
 int main(int argc, char** argv) {
   if(argc == 2) {
-    /* model 1: 3601; U.S., sampled at one arc-second lat/long intervals, 3601 lines and 3601 samples */
-    /* model 3: 1201; world, sampled at three arc-second lat/long intervals, 1201 lines and 1201 samples */
+    HGTParser parser(argv[1], SRTMModel::US);
+    TileExporter exporter(parser);
 
-    HGTParser parser(argv[1], 3601, 3601); /* .hgt are tiles */
-
-    /* coordinates on your tile, e.g. on tile N36W112 */
-    cout << parser.getHeight(42, 42) << endl;
+    exporter.writeSurface("out.jpg", TileSurface::Image);
   }
   else {
     cerr << "error: invalid arguments" << endl;
