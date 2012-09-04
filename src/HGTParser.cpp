8adbd9f54fc6780c454cddb5f67fb8804feabc4a
Daniel J. Hofmann <daniel@trvx.org>
2012-09-04 18:42:51 +0200

    robust statistics

--- src/HGTParser.cpp
+++ src/HGTParser.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <algorithm>
 
 #include "HGTParser.h"
 #include "SRTMModel.h"
@@ -14,51 +15,59 @@ using std::unique_ptr;
 using std::cerr;
 using std::endl;
 using std::for_each;
-using std::min_element;
 
 using namespace SRTMUtil;
 
 
 HGTParser::HGTParser(const string& filename, SRTMModel model) :
-  max(static_cast<const long int>(model)),
-  height(unique_ptr<uint16_t[]>(new uint16_t[max * max]())) {
+  tileSize(static_cast<const long int>(model)),
+  height(unique_ptr<uint16_t[]>(new uint16_t[tileSize * tileSize]())) {
 
+  loadFile(filename);
+  convertEndianess();
+  calculateStatistics();
+}
+
+void HGTParser::loadFile(const string& filename) {
   ifstream file;
 
   try {
     file.exceptions(ios_base::failbit | ios_base::badbit);
     file.open(filename, ios_base::in | ios_base::binary);
-    file.read(reinterpret_cast<char*>(height.get()), max * max * 2);
+    file.read(reinterpret_cast<char*>(height.get()), tileSize * tileSize * 2);
     file.exceptions(ios_base::goodbit);
 
   } catch(const ios_base::failure &e) {
     cerr << "error: could not load file" << endl;
     exit(EXIT_FAILURE);
   }
-
-  convertEndianess();
 }
 
 void HGTParser::convertEndianess() {
-  for_each(height.get(), height.get() + (max * max), [](uint16_t& h){
-      h = (((h & 0xff) << 8) | ((h & 0xff00) >> 8));
+  for_each(height.get(), height.get() + (tileSize * tileSize), [](uint16_t& h){
+    h = (((h & 0xff) << 8) | ((h & 0xff00) >> 8));
   });
 }
 
-int HGTParser::getHeightMin() const {
-  return static_cast<int>(
-      *min_element(height.get(), height.get() + (max * max))
-  );
-}
-
-int HGTParser::getHeightMax() const {
-  /* note: member var named 'max' */
-  int heightMax = 0;
-
+void HGTParser::calculateStatistics() {
   /* filter void 0x8000 values */
-  for_each(height.get(), height.get() + (max * max), [&heightMax](uint16_t h){
-      if((h > heightMax) && (h != 0x8000)) heightMax = h;
+
+  const auto begin = height.get();
+  const auto end = height.get() + (tileSize * tileSize);
+
+  int accu = 0;
+
+  /* max */
+  for_each(begin, end, [&accu](const uint16_t h) {
+    if((h > accu) && (h != 0x8000)) accu = h;
   });
 
-  return heightMax;
+  heightMax = accu;
+
+  /* min */
+  for_each(begin, end, [&accu](const uint16_t h){
+    if((h < accu) && (h != 0x8000)) accu = h;
+  });
+
+  heightMin = accu;
 }
--- src/HGTParser.h
+++ src/HGTParser.h
@@ -3,7 +3,6 @@
 
 #include <cstdint>
 #include <string>
-#include <algorithm>
 #include <memory>
 
 #include "SRTMModel.h"
@@ -27,19 +26,31 @@ class HGTParser final {
 
     /* height or invalid point by specification */
     int getHeight(const long int x, const long int y) const;
-    int getHeightMin() const;
-    int getHeightMax() const;
+    int getHeightMin() const { return heightMin; }
+    int getHeightMax() const { return heightMax; }
 
     /* dimension accessor */
-    long int getMax() const { return max; }
+    long int getTileSize() const { return tileSize; }
 
 
   private:
+    /* load data from .hgt file */
+    void loadFile(const std::string& filename);
+
     /* data is big endian encoded, we have to convert it */
     void convertEndianess();
 
+    /* get useful statistics */
+    void calculateStatistics();
+
     /* defined by the SRTM model specification  */
-    const long int max;
+    const long int tileSize;
+
+    /* min height in this tile */
+    int heightMin;
+
+    /* max height in this tile */
+    int heightMax;
 
     /* actual height data */
     std::unique_ptr<uint16_t[]> height;
@@ -52,7 +63,7 @@ inline int HGTParser::getHeight(const long int x, const long int y) const {
     return 0;
   else
     /* not cache-friendly, but the tiles are not that large, anyway */
-    return static_cast<int>((height.get())[y * max + x]);
+    return static_cast<int>((height.get())[y * tileSize + x]);
 }
 
 
